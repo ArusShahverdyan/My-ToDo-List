0d900870a9eca7cd980a78eb5226c5ebe98a9919
ArusShahverdyan <shahverdyanarus@gmail.com>
2023-05-09 15:25:21 +0400

    changes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,7 @@ import Product from './Product';
 
 function App() {
   return (
-    <div className="App">
-     
-       <Product 
-       name='banabas'
-        price='1'
-       description='Fresh bananas from Ecuador'/>
-             
-      
-    </div>
+   
   );
 }
 
